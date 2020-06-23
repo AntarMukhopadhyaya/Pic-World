c7cc87a774bc553e4c7da81ea0c188e0a8381d84
root <root@localhost.localdomain>
2020-06-23 20:52:18 +0530

    added profile

--- actions/models.py
+++ actions/models.py
@@ -1,4 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    user_img = models.ImageField(upload_to='user/images')
+    interest = models.TextField(max_length=100,default='')
+    birth_date = models.DateField(null=True, blank=True)
